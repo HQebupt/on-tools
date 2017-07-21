9960431098b365130e9b56a900cdc8f03f9e6bc9
HQebupt <qiang.huang1991@gmail.com>
2017-07-21 03:26:29 -0600

    delete docker-compose-mini.yml, use docker-compose.yml

--- manifest-build-tools/post_test.sh
+++ manifest-build-tools/post_test.sh
@@ -198,14 +198,14 @@ post_test_docker() {
     cd $RackHDDir/docker 
     #if clone file name is not repo name, this scirpt should be edited.
     while read -r LINE; do
-        cp docker-compose-mini.yml docker-compose-mini.yml.bak
+        cp docker-compose.yml docker-compose.yml.bak
         for repo_tag in $LINE; do
             repo=${repo_tag%:*}
-            sed -i "s#rackhd/${repo}.*#rackhdmirror/${repo_tag}#g" docker-compose-mini.yml
+            sed -i "s#rackhd/${repo}.*#rackhdmirror/${repo_tag}#g" docker-compose.yml
         done
-        docker-compose -f docker-compose-mini.yml pull --ignore-pull-failures
-        docker-compose -f docker-compose-mini.yml up -d
-        mv docker-compose-mini.yml.bak docker-compose-mini.yml
+        docker-compose -f docker-compose.yml pull --ignore-pull-failures
+        docker-compose -f docker-compose.yml up -d
+        mv docker-compose.yml.bak docker-compose.yml
         waitForAPI
         clean_all_containers
     done < $buildRecord
